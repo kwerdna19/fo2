866db4a41230c6698e2ba3c48e013884a3946f83
kwerdna19 <kuczynskiandrew@yahoo.com>
2023-07-22 20:10:33 -0400

    update footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -11,19 +11,18 @@ const credits = [
 
 export function Footer() {
 
-  return <div className="flex justify-between w-full">
-    <div className="px-3">
-      <a href="https://github.com/kwerdna19/fo2">
-        <BsGithub className="h-6 w-6" />
-      </a>
-    </div>
-    <div className="text-sm text-muted-foreground">
+  return <div className="flex justify-between w-full px-2">
+  <div className="text-sm text-muted-foreground">
     {credits.map((c,i) => {
       return <div key={i}>
         {c}
       </div>
     })}
   </div>
-  <div />
+    <div className="px-3 pt-1">
+      <a href="https://github.com/kwerdna19/fo2">
+        <BsGithub className="h-6 w-6" />
+      </a>
+    </div>
   </div>
 }
