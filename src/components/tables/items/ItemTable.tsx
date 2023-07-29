d6a4a8c53d70768e12ed4414aab1f4927bb5b5cb
Andrew Kuczynski <kuczynskiandrew@gmail.com>
2023-07-28 22:24:54 -0400

    add stats column

--- src/components/tables/items/ItemTable.tsx
+++ src/components/tables/items/ItemTable.tsx
@@ -28,11 +28,46 @@ import { getSortButton } from "~/components/SortButton";
 import { GoldCount } from "~/components/GoldCount"
 import { DroppedByList } from "./DroppedByList"
 import { cn } from "~/utils/styles"
+import { type Item } from "@prisma/client"
 
 
 type Data = RouterOutputs['item']['getAll']
 export type Datum = Data[number]
 const columnHelper = createColumnHelper<Datum>()
+
+function ItemStats(props: { stats: Pick<Item, 'str' | 'agi' | 'armor' | 'int' | 'sta'>, className?: string }) {
+
+
+  const { className, stats: inputItem } = props
+
+  const armor = inputItem.armor
+
+  const stats = (['str', 'agi', 'int', 'sta'] as const).filter(s => inputItem[s] !== null).map(s => {
+    return {
+      stat: s.toUpperCase(),
+      value: inputItem[s]!
+    }
+  })
+  //.sort((a, b) => b.value - a.value)
+
+
+  if(stats.length === 0 && !armor) {
+    return null
+  }
+
+  return (<div className={cn(className, "text-sm")}>
+    {stats.length > 0 ? <div className="bg-slate-200 w-16 space-y-1.5 rounded-md p-1 mb-1">
+      {stats.map(({ stat, value }) => {
+            return (<div key={stat} className="flex justify-between">
+            <div className="bg-slate-300 w-6 text-center rounded-sm">{value > 0 ? `+${value}` : value}</div>
+            <div className="flex-1 text-center pl-1">{stat}</div>
+        </div>)
+      })}
+    </div> : null}
+    {armor ? <div className="p-1"><span>{armor > 0 ? `+${armor}` : armor}</span> <span>Armor</span></div> : null}
+  </div>)
+
+}
 
 export const columns = [
   columnHelper.display({
@@ -48,6 +83,11 @@ export const columns = [
   }),
   columnHelper.accessor('levelReq', {
     header: getSortButton('Level Req')
+  }),
+  columnHelper.display({
+    id: 'stats',
+    header: 'Stats',
+    cell: ({ row }) => <ItemStats stats={row.original} />
   }),
   columnHelper.accessor('sellPrice', {
     header: getSortButton('Sell Price'),
