e3afe5f5d18b0919a0009cddea26d4b61a6bd897
kwerdna19 <kuczynskiandrew@yahoo.com>
2023-10-26 19:23:41 -0400

    add route for schema

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -12,3 +12,11 @@ export const getListOfImages = (spriteType: string) => {
   const images = filenames.map(f => path.join('/', 'sprites', spriteType, f).replace(/\\/g, '/'))
   return images
 }
+
+export function setCorsHeaders(res: Response) {
+  res.headers.set("Access-Control-Allow-Origin", "*");
+  res.headers.set("Access-Control-Request-Method", "*");
+  res.headers.set("Access-Control-Allow-Methods", "OPTIONS, GET, POST");
+  res.headers.set("Access-Control-Allow-Headers", "*");
+  return res
+}
