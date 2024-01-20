e227d59069b85e46850f7c7032e5902d900cd665
kwerdna19 <kuczynskiandrew@yahoo.com>
2024-01-20 17:23:37 -0500

    lint issue

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -7,7 +7,7 @@ export const dynamic = "force-dynamic"
 export default async function Login() {
 
   const session = await auth()
-  if(session && session.user) {
+  if(session?.user) {
     redirect('/')
   }
 
