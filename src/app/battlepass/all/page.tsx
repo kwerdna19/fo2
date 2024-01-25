bd836ed4c23c1b366831d742b661262015a95e5d
kwerdna19 <kuczynskiandrew@yahoo.com>
2024-01-25 14:55:54 -0500

    add reval to bps page

--- src/app/battlepass/all/page.tsx
+++ src/app/battlepass/all/page.tsx
@@ -4,6 +4,8 @@ export const metadata = {
   title: 'All Battlepasses'
 }
 
+export const revalidate = 86400
+
 export default async function BattlePasses() {
   const passes = await getAllBattlePasses()
   return <pre>{JSON.stringify(passes, null, 2)}</pre>
