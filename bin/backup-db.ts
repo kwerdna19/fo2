dc60c8ec6d5326678100d4bb4c6ffbdeb40399b9
kwerdna19 <kuczynskiandrew@yahoo.com>
2024-01-26 11:18:25 -0500

    add db backup cmd

---
+++ bin/backup-db.ts
@@ -0,0 +1,38 @@
+import { join } from 'path'
+import { writeFile } from 'fs/promises';
+import { execFileSync } from 'child_process';
+import { format } from 'date-fns';
+import { existsSync } from 'fs';
+
+const outputDir = './bin/database-backups'
+
+const main = async () => {
+
+  const host = process.env.POSTGRES_HOST
+  const user = process.env.POSTGRES_USER
+  const db = process.env.POSTGRES_DATABASE
+  const password = process.env.PGPASSWORD
+
+  if(!host || !user || !db || !password) {
+    throw new Error('Missing DB env variables')
+  }
+
+  const backupOutputPath = join(outputDir, `${format(new Date(), 'MM-dd-yyyy')}_fo2db.bak`)
+
+  if(existsSync(backupOutputPath)) {
+    console.log('Backup already exists.')
+    return
+  }
+
+  process.env.PGPASSWORD = password
+
+  const args = `-h ${host} -Fc -O -U ${user} ${db}`
+  const output = execFileSync('pg_dump', args.split(' '));
+
+  await writeFile(backupOutputPath, output)
+
+  
+}
+
+
+void main()
