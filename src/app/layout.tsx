9b7328dcc82c3d9b0fda0ee60d99cda3d2385775
kwerdna19 <kuczynskiandrew@yahoo.com>
2024-01-30 22:05:24 -0500

    remove speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import "leaflet/dist/leaflet.css";
 import { type Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -57,7 +56,6 @@ export default function RootLayout({
 				</ThemeProvider>
 
 				<Analytics />
-				<SpeedInsights />
 			</body>
 		</html>
 	);
